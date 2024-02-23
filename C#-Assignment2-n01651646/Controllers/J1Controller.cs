e08952140b74063b38b609b2cdb0e3668e487e08
Disha Kolapate <dishakolapate@gmail.com>
2024-02-22 22:16:46 -0500

    Changes made in J1 controller.

--- C#-Assignment2-n01651646/Controllers/J1Controller.cs
+++ C#-Assignment2-n01651646/Controllers/J1Controller.cs
@@ -10,31 +10,27 @@ namespace C__Assignment2_n01651646.Controllers
     public class J1Controller : ApiController
     {
         /// <summary>
-        /// This API returns the total calories consumed according to the menu.  
+        /// This API returns the remaining number of cupcakes after they have been distributed within the 28 students.  
         /// </summary>
-        /// <param name="burger">index burger choice</param>
-        /// <param name="drink">index drink choice</param>
-        /// <param name="side">index side order choice</param>
-        /// <param name="dessert">index dessert choice</param>
-        /// <returns>String message giving the computed total Calories of the meal</returns>
+        /// <param name="regular">index number of regular boxes</param>
+        /// <param name="drink">index number of small boxes </param>
+
+        /// <returns>Integer returns the number of remainig Cupcakes</returns>
         /// <example>
-        /// Get: /api/J1/Menu/4/4/4/4        -> total calorie count is 0
-        /// GET: /api/J1/Menu/1/2/3/4       ->total calorie count is 691
+        /// Get: /api/J1/Party/2/4       -> 0
+        /// GET: /api/J1/Party/2/5     -> 3
         /// </example>
-        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        [Route("api/J1/Party/{regular}/{small}")]
         [HttpGet]
-        public string Menu(int burger, int drink, int side, int dessert)
+        public int Party(int regular, int small)
         {
-            List<int> burgerCal = new List<int> { 461, 431, 420, 0 };
-            List<int> drinkCal = new List<int> { 130, 160, 118, 0 };
-            List<int> sideCal = new List<int> { 100, 57, 70, 0 };
-            List<int> dessertCal = new List<int> { 167, 266, 75, 0 };
+            if(regular>=0 && small>=0) {
+                int totalCupcakes = regular * 8 + small * 3;
+                int remainder = totalCupcakes % 28;
+                return remainder;
+            }
+            else return 0;
 
-            int total = burgerCal[burger - 1] + drinkCal[drink - 1] + dessertCal[dessert-1] + sideCal[side-1];
-
-            
-
-            return "The total Calorie count is " + total;
         }
     }
 }
